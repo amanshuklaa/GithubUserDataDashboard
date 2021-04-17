95a2585185d34732ec267c86f886a023ceed4e2f
System Administrator <root@amans-MacBook-Pro.local>
2021-04-18 01:04:35 +0530

    dashboard for git

---
+++ src/components/Profile.js
@@ -0,0 +1,57 @@
+import React ,{useState} from 'react'
+import DisplayTable from './DisplayTable'
+
+
+const Profile = ()=>{
+    const [data,setData] = useState({})
+    const [username, setUserName] = useState("")
+    const [repositories,setRepositories] = useState([])
+
+    const handleUserNameChange = (e)=>{
+        let repoName = e.target.value;
+        setUserName(repoName)
+        
+    }
+    const handleSubmit =async (e)=>{
+        e.preventDefault();
+        const profile = await fetch(`https://api.github.com/users/${username}`);
+        const profileJson = await profile.json();
+        // console.log(profileJson);
+    
+        const repositories = await fetch(profileJson.repos_url);
+        const repoJson = await repositories.json();
+        // console.log(repoJson);
+        if(profileJson){
+            setData(profileJson)
+            setRepositories(repoJson)
+        }
+    }
+    return (
+        <>
+        <div style ={{padding: 20}}>
+            <div className ="ui search">
+            <div className="ui icon input">
+            <i className="search icon">
+
+            </i>
+            <input 
+            className="prompt"
+            placeholder="search username here"
+            type="text" value={username} onChange={handleUserNameChange}></input>
+
+            </div>
+            <button className="ui primary button"
+             type="submit" onClick={handleSubmit}
+            >
+                <i className="github icon"></i>
+                Search
+
+            </button>
+            <DisplayTable data={data} repositories={repositories}/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Profile
